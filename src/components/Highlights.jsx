f27208b2b379efb181dec7ee2a5a4e5bc373c3c0
TuShArBhArDwA <tusharbhardwaj2617@gmail.com>
2024-12-13 22:50:59 +0530

    done

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,34 +1,26 @@
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-// import { rightImg, watchImg } from "../../public/assets/images";
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { rightImg, watchImg } from "../utils"
 
-import VideoCarousel from "./VideoCarousel";
+import VideoCarousel from './VideoCarousel';
 
 const Highlights = () => {
   useGSAP(() => {
-    gsap.to('#title', { opacity: 1, y: 0 });
-    gsap.to('.link', { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
-  }, []);
+    gsap.to('#title', { opacity: 1, y: 0 })
+    gsap.to('.link', { opacity: 1, y: 0, duration: 1, stagger: 0.25 })
+  }, [])
 
   return (
-    <section
-      id="how-it-works"
-      className="w-screen overflow-hidden h-full common-padding bg-black flex items-center justify-center"
-    >
-      <div className="screen-max-width text-center">
-        <div className="mb-12 w-full md:flex items-center justify-center">
-          <h1
-            id="title"
-            className="section-heading text-white text-6xl font-bold"
-          >
-            How It Works
-          </h1>
+    <section id="how-it-works" className="w-screen overflow-hidden h-full common-padding bg-black">
+      <div className="screen-max-width">
+        <div className="mb-12 w-full md:flex items-end justify-between">
+          <h1 id="title" className="section-heading">How It Works</h1>
         </div>
 
         <VideoCarousel />
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Highlights;
+export default Highlights
